db2dfcda87925ceeaea28ae3ef227e16ccac848d
PeerawatK <118897126+PeerawatK@users.noreply.github.com>
2026-02-17 02:55:32 +0700

    list implement

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
+import { ListView } from '@/components/refine-ui/views/list-view'
+import { Search } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import React, { useState } from 'react'
 
 const SubjectsList = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
   return (
-    <div>
-      
-    </div>
+    <ListView>
+        <Breadcrumb/>
+        <h1 className='page-title'>Subjects</h1>
+
+        <div className='intro-row'>
+            <p>Quick access to essential metrics amd management tools.</p>
+            <div className='actions-row'>
+                <div className='search-field'>
+                    <Search className='search-icon' />
+                <Input
+                    type='text'
+                    placeholder='Search by name ...'
+                    className="pl-10 w-full"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                </div>
+                
+            </div>
+                
+        </div>
+    </ListView>    
   )
 }
 
